487979a356778d378a42587c1852e0b685dbb2f8
rbiencarlo <cc.biencarlo@gmail.com>
2023-09-11 17:46:33 +0800

    made changes on form-test

--- app/dashboard/form-test/page.tsx
+++ app/dashboard/form-test/page.tsx
@@ -5,6 +5,9 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import MenuItem from "@mui/material/MenuItem";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CenterFocusStrong from "@mui/icons-material/CenterFocusStrong";
+import "./styles.css";
 
 const formtypes = [
   {
@@ -18,6 +21,25 @@ const formtypes = [
   {
     value: "Health Center Referral",
     label: "Health Center Referral",
+  },
+];
+
+const philc_types = [
+  {
+    value: "Dependent",
+    label: "Dependent",
+  },
+  {
+    value: "Senior",
+    label: "Senior",
+  },
+  {
+    value: "NHTs",
+    label: "NHTs",
+  },
+  {
+    value: "APs",
+    label: "APs",
   },
 ];
 
@@ -52,32 +74,142 @@ function page() {
         autoComplete="off"
       >
         <div>
-          <TextField
-            id="select-form-type"
-            select
-            label="Select"
-            defaultValue="Health Center Referral"
-            helperText="Please select form type"
-          />
-          {formtypes.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-          <TextField
-            id="last-name"
-            label="Last Name"
-            margin="normal"
-            style={{ width: "40%" }}
-            variant="filled"
-          />
-          <TextField
-            id="last-name"
-            label="Last Name"
-            margin="normal"
-            style={{ width: "40%" }}
-            variant="filled"
-          />
+          <div className="w-full flex place-content-stretch mt-4">
+            <TextField
+              id="select-form-type"
+              select
+              label="Form Type"
+              defaultValue="Health Center Referral"
+              helperText="Please select form type"
+              margin="normal"
+              style={{ width: "100%" }}
+              required
+            >
+              {formtypes.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button
+              size="large"
+              startIcon={<CenterFocusStrong sx={{ color: "black" }} />}
+              variant="outlined"
+              sx={{ height: 55, width: "600px" }}
+              className="red-button mt-4 mr-5 py-2 px-10 hover:bg-red-800 hover:border-transparent rounded-sm transition-all "
+            >
+              Use OCR (Scan Document)
+            </Button>
+            <Button
+              size="large"
+              variant="outlined"
+              sx={{ height: 55, width: "300px" }}
+              className="mt-4 py-2 px-10 hover:bg-red-800 hover:border-transparent rounded-sm transition-all "
+            >
+              Create Document
+            </Button>
+          </div>
+          <div className="flex-nowrap flex">
+            <div className="w-1/5 pr-4">
+              <TextField
+                id="last-name"
+                label="Last Name"
+                margin="normal"
+                variant="outlined"
+                helperText="Please enter name"
+                style={{ width: "100%" }}
+                required
+              />
+            </div>
+            <div className="w-1/5 pr-4">
+              <TextField
+                id="first-name"
+                label="First Name"
+                margin="normal"
+                variant="outlined"
+                required
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className="w-1/5 pr-4">
+              <TextField
+                id="middle-name"
+                label="Middle Name"
+                margin="normal"
+                variant="outlined"
+                required
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className="w-1/5 pr-4">
+              <TextField
+                id="philhealth-no"
+                label="Philhealth No."
+                margin="normal"
+                variant="outlined"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className="w-1/5">
+              <TextField
+                id="select-philc-type"
+                select
+                label="ID Category"
+                defaultValue="Health Center Referral"
+                helperText="Select ID category"
+                margin="normal"
+                style={{ width: "100%" }}
+              >
+                {philc_types.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </div>
+          </div>
+          <div className="flex-nowrap flex">
+            <div className="w-2/3 pr-4">
+              <TextField
+                id="address"
+                label="Address"
+                margin="normal"
+                variant="outlined"
+                style={{ width: "100%" }}
+                required
+              />
+            </div>
+            <div className="w-1/3">
+              <TextField
+                id="hc-ggg-no"
+                label="Healthcard GGG No."
+                margin="normal"
+                variant="outlined"
+                style={{ width: "100%" }}
+              />
+            </div>
+          </div>
+          <div className="flex-nowrap flex">
+            <div className="w-3/5 pr-4">
+              <TextField
+                id="contact-no"
+                label="Contact No."
+                margin="normal"
+                variant="outlined"
+                style={{ width: "100%" }}
+                required
+              />
+            </div>
+            <div className="w-2/5">
+              <TextField
+                id="hc-ggg-no"
+                label="Healthcard GGG No."
+                margin="normal"
+                variant="outlined"
+                style={{ width: "100%" }}
+              />
+            </div>
+          </div>
         </div>
       </Box>
     </div>
